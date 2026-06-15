56992739dfe6bef2314e630d0d0ce18df77bf5ef
omorishaoba-alt <omorishaoba@gmail.com>
2026-06-15 16:04:53 +0100

    Add async sha256 function for hashing messages

--- core/sha256.js
+++ core/sha256.js
@@ -1,1 +1,7 @@
-
+async function sha256(message) {
+  const data = new TextEncoder().encode(message);
+  const hash = await crypto.subtle.digest("SHA-256", data);
+  return [...new Uint8Array(hash)]
+    .map(b => b.toString(16).padStart(2, "0"))
+    .join("");
+}
